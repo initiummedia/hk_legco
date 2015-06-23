8e467852228979251ff93cf0f1a2ac8c039c68db
HU Pili <hpl1989@gmail.com>
2015-06-23 16:14:52 +0800

    prepare data

--- scripts/download_voting_records.py
+++ scripts/download_voting_records.py
@@ -3,19 +3,30 @@ from lxml import etree
 import requests
 from pyquery import PyQuery as pq
 import pandas as pd
+import os
+from os import path
+import sys
+
+# from scripts import config
+import config
 
 seed_pages = [
     'http://www.legco.gov.hk/general/english/counmtg/yr12-16/mtg_1213.htm',
     'http://www.legco.gov.hk/general/english/counmtg/yr12-16/mtg_1314.htm',
     'http://www.legco.gov.hk/general/english/counmtg/yr12-16/mtg_1415.htm'
 ]
+
 def crawl_seed(seed):
     d = pq(seed)
     return d('a').map(lambda i, a: a.attrib.get('name', None)).filter(lambda i, s: s.startswith('cm20'))
+
 meetings = []
 for seed_page in seed_pages:
     meetings.extend(crawl_seed(seed_page))
-print(meetings)
+
+with open(path.join(config.DIR_DATA_ROOT, 'meetings.txt'), 'w') as fp:
+    for m in meetings:
+        fp.write('%s\n' % m)
 
 def crawl_xml(meeting):
     # This logic is translated from the official JS code
@@ -28,15 +39,22 @@ def crawl_xml(meeting):
     url = '%(prefix)s/%(yr)s/chinese/counmtg/voting/cm_vote_20%(yy)02d%(mm)02d%(dd)02d.xml' % locals()
     return requests.get(url)
 
+print('Parsed %d meetings from the root page' % len(meetings))
+
 vote_xmls = []
 for m in meetings:
-    vote_xmls.append(crawl_xml(m))
-    print('progress: %s/%s %s' % (len(vote_xmls), len(meetings), '#' * len(vote_xmls)))
+    r = crawl_xml(m)
+    print('Crawling %s' % m)
+    # print('progress: %s/%s %s' % (len(vote_xmls), len(meetings), '#' * len(vote_xmls)))
     sys.stdout.flush()
+    with open(path.join(config.DIR_VOTING_RECORDS_RAW, '%s.xml' % m), 'w') as fp:
+        if r.ok:
+            fp.write(str(r.content))
+            vote_xmls.append(r.content)
 
-vote_xmls = filter(lambda r: r.ok, vote_xmls)
-vote_xmls = [r.content for r in vote_xmls]
-print(len(vote_xmls))
+# vote_xmls = filter(lambda r: r.ok, vote_xmls)
+# vote_xmls = [r.content for r in vote_xmls]
+print('Collected %d voting record XMLs in total' % len(vote_xmls))
 
 # Information fields, useful for reviewing the result
 info_fields = ['vote-date', 'vote-time', 'motion-en', 'mover-en', 'mover-type', 'vote-separate-mechanism']
@@ -70,5 +88,5 @@ def clean_record(t):
 records = [clean_record(r) for r in records]
 
 df = pd.DataFrame(records, columns = ['topic_id', 'member_id', 'vote'] + info_fields)
-df.to_csv('records-all-with-info.csv', encoding='utf-8')
+df.to_csv(path.join(config.DIR_DATA_ROOT, 'records-all-with-info.csv'), encoding='utf-8')
 df.head()
