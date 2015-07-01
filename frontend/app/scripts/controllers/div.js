6f40af077083e3bb441aa27672dec08ea68ed934
HU Pili <hpl1989@gmail.com>
2015-07-01 15:19:54 +0800

    load mv relation data and transdicts

--- frontend/app/scripts/controllers/div.js
+++ frontend/app/scripts/controllers/div.js
@@ -17,6 +17,23 @@ angular.module('frontendApp')
     $scope.msg = 'hello';
 
     $http.get('/api/transdict-mover.json').success(function(data){
+      $scope.transDictMover = data;
       console.log(data);
+    }).error(function(data){
+      console.log('loading failure');
+    });
+
+    $http.get('/api/transdict-voter.json').success(function(data){
+      $scope.transDictVoter = data;
+      console.log(data);
+    }).error(function(data){
+      console.log('loading failure');
+    });
+
+    $http.get('/api/mv-relation.json').success(function(data){
+      $scope.mvRelation = data;
+      console.log(data);
+    }).error(function(data){
+      console.log('loading failure');
     });
   }]);
