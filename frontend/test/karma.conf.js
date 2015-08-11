886b344ae18e729bd83c9373249364c19cd82430
HU Pili <hpl1989@gmail.com>
2015-08-12 00:26:30 +0800

    add opencc dep

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -39,6 +39,8 @@ module.exports = function(config) {
       'bower_components/angular-busy/dist/angular-busy.js',
       'bower_components/ngDialog/js/ngDialog.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/qrcode-generator/js/qrcode.js',
+      'bower_components/angular-qrcode/angular-qrcode.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
