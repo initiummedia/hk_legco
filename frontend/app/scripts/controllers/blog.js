0350970ce97abd29c9fac1a312e7d922cf4dad9c
HU Pili <hpl1989@gmail.com>
2015-08-11 23:07:16 +0800

    migrate video playing script

--- frontend/app/scripts/controllers/blog.js
+++ frontend/app/scripts/controllers/blog.js
@@ -18,4 +18,16 @@ angular.module('frontendApp')
     //TODO: add any control logics here
     $('#shareVideoToFacebookAnchor').attr('href', 'https://facebook.com/');
 
+    // Special configuration for touchscreens
+    var is_touch_device = 'ontouchstart' in document.documentElement;
+    if (is_touch_device) {
+      //TODO
+    };
+
+    // Enable click-to-play for videos
+    var video = document.getElementById('introVideo');
+    video.addEventListener('click', function(){
+      this.paused?this.play():this.pause();
+    }, false);
+
   });
