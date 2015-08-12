939827be699d88a8dc4cb423d5f9b07ba55cb66c
HU Pili <hpl1989@gmail.com>
2015-08-12 15:13:09 +0800

    xdomain via bower

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -57,6 +57,7 @@ module.exports = function(config) {
       'bower_components/angulartics/src/angulartics-inspectlet.js',
       'bower_components/angulartics/src/angulartics-newrelic-insights.js',
       'bower_components/angulartics-google-analytics/dist/angulartics-google-analytics.min.js',
+      'bower_components/xdomain/dist/xdomain.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
