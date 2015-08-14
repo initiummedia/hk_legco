b30e103c47dab564c2a6ab4222e2e7dcfbd99048
HU Pili <hpl1989@gmail.com>
2015-08-15 00:45:05 +0800

    separate config functions

--- frontend/app/scripts/services/legcoconfig.js
+++ frontend/app/scripts/services/legcoconfig.js
@@ -10,10 +10,22 @@
 angular.module('frontendApp')
   .service('LegcoConfig', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
-    return {
-      getRealPath: function(path){
-        var pathScheme = $('meta[name="pathScheme"]').first().attr('content');
+    var config = {
+      getBaseURL: function(){
         var baseURL = $('base').first().attr('href');
+        return baseURL;
+      },
+      getApiBaseURL: function(){
+        var baseURL = config.getBaseURL();
+        return baseURL + 'api/';
+      },
+      getPathScheme: function(){
+        var pathScheme = $('meta[name="pathScheme"]').first().attr('content');
+        return pathScheme;
+      },
+      getRealPath: function(path){
+        var baseURL = config.getBaseURL();
+        var pathScheme = config.getPathScheme();
         //console.log('pathScheme: ' + pathScheme);
         //console.log('baseURL: ' + baseURL);
         var rp = baseURL + pathScheme + path;
@@ -22,5 +34,6 @@ angular.module('frontendApp')
         }
         return rp;
       }
-    }
+    };
+    return config;
   });
